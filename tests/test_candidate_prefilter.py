49c142bdaaf6cc0e01a9c054e31a4f3687f6c088
ozand <ozand.ru@gmail.com>
2026-09-26 22:07:58 +0300

    test(scanner): cover anchor alternatives and key recognizers

--- tests/test_candidate_prefilter.py
+++ tests/test_candidate_prefilter.py
@@ -33,8 +33,35 @@ def test_missing_anchor_fails_closed(monkeypatch):
 
 def test_each_scanner_rule_has_declared_anchor():
     rules = (*ps.STANDALONE_PATTERNS,)
+    rules = (*ps.STANDALONE_PATTERNS, ps.SecretPattern("_env_secret_kv", ps._ENV_SECRET_KV_RE, "env"), ps.SecretPattern("_json_secret_key", ps._JSON_SECRET_KEY_RE, "json"))
     for rule in rules:
-        assert ps.SCANNER_ANCHORS.get(rule.name), rule.name
+        anchors = ps.SCANNER_ANCHORS.get(rule.name)
+        assert anchors, rule.name
+        for sample in _positive_samples(rule.name):
+            matcher = (ps._JSON_SECRET_KEY_RE if rule.name == "_json_secret_key"
+                       else ps._ENV_SECRET_KV_RE if rule.name == "_env_secret_kv"
+                       else rule.pattern)
+            assert matcher.search(sample), (rule.name, sample)
+            assert ps._has_scan_candidate(sample, ps.STANDALONE_PATTERNS), (rule.name, sample)
+
+
+def _positive_samples(name):
+    return {
+        "eeepc_agent_path": ["/etc/eeepc-agent"],
+        "openai_secret_key": ["sk-" + "A" * 24],
+        "github_token": ["ghp_" + "A" * 20, "github_pat_" + "A" * 20],
+        "bearer_token": ["Bearer " + "A" * 20],
+        "aws_access_key": ["AKIA" + "A" * 16],
+        "slack_token": ["xoxb-" + "A" * 12],
+        "basic_auth": ["Basic " + "A" * 16, "Authorization : Basic " + "A" * 16],
+        "url_credentials": ["http://u:p@h", "https://u:p@h"],
+        "private_key_header": ["-----BEGIN PRIVATE KEY-----"],
+        "structural_reasoning_content": ['"reasoning_content"', "'reasoning_content'"],
+        "structural_messages": ['"messages":', "'messages':"],
+        "structural_prompt": ['"prompt": {', "'prompt': {"],
+        "_env_secret_kv": ["DB_PASS=abcdefghijk", "AUTH=abcdefghijk", "ACCOUNT_KEY=abcdefghijk", "GH_TOKEN=abcdefghijk"],
+        "_json_secret_key": ['{"my_token_value":"abcdefghijk"}'],
+    }[name]
 
 
 def test_candidate_guard_has_no_false_negatives_for_known_pattern_corpus():
